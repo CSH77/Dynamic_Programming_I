60dbbb797e5e04c827b55f9b21fe4c4ca3ab2480
SeungHwan <seunghwanchoi@outlook.com>
2021-10-02 17:38:28 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+
+};
+
+int main()
+{
+    Solution obj;
+
+    cout << "hello world!" << endl;
+
+    return 0;
+}
+
+
+
