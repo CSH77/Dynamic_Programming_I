47e985bd7d620df01bb01a1c34556d06a9d9cfcc
SeungHwan <seunghwanchoi@outlook.com>
2021-12-12 01:14:00 -0800

    DP solution

--- main.cpp
+++ main.cpp
@@ -1,93 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <set>
+#include <map>
+#include <array>
+
 
 using namespace std;
 
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-<<<<<<< HEAD
-        if(nums.size() == 0)
-            return 0;
+    int deleteAndEarn(vector<int>& nums) {
+        int cache[10001] = {0};
 
-        if(nums.size() == 1)
-            return nums[0];
+        for(int num : nums)
+            cache[num] += num;
 
-        if(nums.size() == 2)
-            return max(nums[0], nums[1]);
-
-
-        int a = rob_simple(nums, 0, nums.size() - 2);
-        int b = rob_simple(nums, 1, nums.size() - 1);
-
-        return max(a, b);
-    }
-
-    int rob_simple(vector<int> nums, int start, int end){
-        int t1 = 0;
-        int t2 = 0;
-
-        for(int i = start; i <= end; i++){
-            int temp = t1;
-            nums[i] = max(nums[i] + t2, t1);
-            t1 = nums[i];
-            t2 = temp;
+        int take = 0, skip = 0;
+        for(int i=0; i<10001; i++)
+        {
+            int take_i = skip + cache[i];
+            int skip_i = max(take, skip);
+            take = take_i;
+            skip = skip_i;
         }
-        int answer = nums[end];
-
-        return answer;
-=======
-
-        if(nums.size() < 1)
-            return 0;
-
-        //only one item, return the item.
-        if(nums.size() < 2)
-            return nums[0];
-
-        //circle, last one can't be robbed because first one is connected to last one.
-        //start from first one, exclude last one.
-        int a = rob_simple(nums, 0, nums.size() -2 );
-
-        //sttart from second one
-        int b = rob_simple(nums, 1, nums.size() - 1 );
-        return max(a,b);
-    }
-
-    int rob_simple(vector<int>& nums, int start, int end){
-        int t1 = 0; //latest max
-        int t2 = 0; //olderest max
-
-        for(int i = start; i <= end; i++){
-            int temp = t1; //backup current max
-            int current = nums[i];
-            t1 = max(current + t2, t1);
-            t2 = temp; //keep previous max
-        }
-        return t1;
->>>>>>> 4e6010517ccc819fcf84759bbb6472491a2ea5b7
+        return take;
     }
 };
+
 
 int main()
 {
     Solution obj;
-<<<<<<< HEAD
-    // vector<int> input = {2,3,2}; //expect 3
-    // vector<int> input = {0}; //expect 0;
-    vector<int> input = {200,3,140,20,10}; //expect 340;
-=======
-    //vector<int> input = {2,3,2}; //expect 3
-    vector<int> input = {1,2,3,1}; //expect 4
+    vector<int> input  = {2,2,3,3,3,4}; //expect 9;
+    // vector<int> input = {1}; //expect 1;
+    // vector<int> input = {3,1}; //expect 4;
+    // vector<int> input = {3,4,2}; //expect 6;
+    // vector<int> input = {1,1,1,2,4,5,5,5,6}; //expect 18;
 
->>>>>>> 4e6010517ccc819fcf84759bbb6472491a2ea5b7
-
-    cout << obj.rob(input);
+    cout << obj.deleteAndEarn(input);
 
     return 0;
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 4e6010517ccc819fcf84759bbb6472491a2ea5b7
